ec622957332e3d9f8f39adeff93e108fe800c86d
Anthony Chang <roykillany@gmail.com>
2015-01-07 10:25:38 -0500

    Adds Sessions Controller and completes Application Controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(user_params[:username], user_params[:password])
+    if @user
+      log_in(@user)
+      redirect_to users_url
+    else
+      flash.now[:errors] = @user.errors.full_messages, status: 422
+      render :new
+    end
+  end
+
+  def destroy
+    current_user.reset_session_token!
+    session[:token] = nil
+    redirect_to new_session_url
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ActiveRecord::Base
   after_initialize :ensure_session_token
 
   def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    return nil unless user && user.is_password?(password)
+    user
+  end
 
   def self.generate_session_token
     SecureRandom.urlsafe_base64
