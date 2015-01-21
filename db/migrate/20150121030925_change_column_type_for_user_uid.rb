0da87f14522d96c0a13879fcc72fa296f1006582
Anthony Chang <roykillany@gmail.com>
2015-01-20 22:12:39 -0500

    Changes column type for UID.

---
+++ db/migrate/20150121030925_change_column_type_for_user_uid.rb
@@ -0,0 +1,5 @@
+class ChangeColumnTypeForUserUid < ActiveRecord::Migration
+  def change
+    change_column :users, :uid, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150121024443) do
+ActiveRecord::Schema.define(version: 20150121030925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -156,7 +156,7 @@ ActiveRecord::Schema.define(version: 20150121024443) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string   "provider"
-    t.integer  "uid"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
