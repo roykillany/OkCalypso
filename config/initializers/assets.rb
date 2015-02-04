e211c6ae84eab350e74230f6567afbd172804828
Anthony Chang <roykillany@gmail.com>
2015-02-04 10:04:18 -0500

    First resize for 1000px max-width.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,8 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( application-1000px.css )
+Rails.application.config.assets.precompile += %w( application-mobile.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
